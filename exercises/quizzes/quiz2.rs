c546c9d20f42453c8008e368dbb751c905cb3495
1631j <73850091+1631j@users.noreply.github.com>
2024-12-13 19:40:17 +0300

    quiz2 done

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -28,6 +28,36 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut output = Vec::new();
+
+        for (string, command) in input {
+            // Create the new string.
+            let new_string = match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => string + &"bar".repeat(n),
+            };
+
+            // Push the new string to the output vector.
+            output.push(new_string);
+        }
+
+        output
+    }
+
+    // Equivalent to `transform` but uses an iterator instead of a loop for
+    // comparison. Don't worry, we will practice iterators later ;)
+    pub fn transformer_iter(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .into_iter()
+            .map(|(string, command)| match command {
+                Command::Uppercase => string.to_uppercase(),
+                Command::Trim => string.trim().to_string(),
+                Command::Append(n) => string + &"bar".repeat(n),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -38,6 +68,9 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
+
+    use super::my_module::transformer_iter;
     use super::Command;
 
     #[test]
