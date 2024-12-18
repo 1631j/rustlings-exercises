988c1810a27897b78739dda290b52682b6ba7ebb
1631j <73850091+1631j@users.noreply.github.com>
2024-12-18 11:29:06 +0300

    16-lifetimes <'a>

--- exercises/16_lifetimes/lifetimes2.rs
+++ exercises/16_lifetimes/lifetimes2.rs
@@ -11,10 +11,14 @@ fn main() {
     // TODO: Fix the compiler error by moving one line.
 
     let string1 = String::from("long string is long");
+    // SOLUTION 1- Removing the string2 to the outside 
+    // let string2 = String::from("xyz");
     let result;
     {
         let string2 = String::from("xyz");
-        result = longest(&string1, &string2);
+         result = longest(&string1, &string2);
+         // SOLUTION 2- Moving println! to inside 
+         println!("The longest string is '{result}'");
     }
-    println!("The longest string is '{result}'");
+    // println!("The longest string is '{result}'");
 }
